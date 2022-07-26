0920e8e1cf28ee64bda5b41791bb385756b48fb2
ShamilUmaev <shamil.umaev@hotmail.com>
2022-07-26 22:05:27 +0200

    Finished the section time-to-get-strong

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,4 +1,15 @@
 <template>
+<div class="outer-container">
+  <div class="time-to-get-strong">
+    <div class="text-content-time-to-get-strong">
+      <h2>The time to get <span class="bold">strong</span> has come</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra amet placerat mauris elementum dignissim.</p>
+      <button class="all-btn">Contact Us</button>
+    </div>
+    <img src="../assets/workout-vector-1.png" alt="">
+  </div>
+  
+</div>
 </template>
 
 <script>
@@ -7,6 +18,33 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.outer-container {
+  width: 80%;
+  margin: 0 auto;
+}
+.time-to-get-strong {
+  width: 100%;
+  display: flex;
+  align-items: center;
+}
+.text-content-time-to-get-strong {
+  width: 50%;
+}
+.text-content-time-to-get-strong h2 {
+  font-size: 4rem;
+}
+.text-content-time-to-get-strong p {
+  width: 80%;
+  margin-top: 30px;
+}
+.all-btn {
+  background: #4B5092;
+  color: #fff;
+  font-size: .9rem;
+  width: 30%;
+  height: 45px;
+  border-radius: 35px;
+  border: none;
+}
 </style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -16,27 +16,27 @@ export default {
 </script>
 
 <style scoped>
-    nav {
-        width: 90%;
-        display: flex ;
-        justify-content: space-between;
-        margin: 30px auto;
-    }
-    h2 {
-        font-size: 2rem;
-    }
-    ul {
-        width: 30%;
-        list-style: none;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-    }
-    a:hover {
-        color: #e8e189;
-    }
-    a {
-        text-decoration: none;
-        color: #111235;
-    }
+nav {
+    width: 80%;
+    display: flex ;
+    justify-content: space-between;
+    margin: 30px auto;
+}
+h2 {
+    font-size: 2rem;
+}
+ul {
+    width: 25%;
+    list-style: none;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+a:hover {
+    color: #e8e189;
+}
+a {
+    text-decoration: none;
+    color: #111235;
+}
 </style>
